190e1bff572edba1c870a68460ab580475fdf6f8
Nicholas Stevens <nstevens@nanick.org>
2022-05-17 18:05:10 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -5,20 +5,18 @@ if([Security.Principal.WindowsPrincipal]::New([Security.Principal.WindowsIdentit
     [System.Console]::SetBufferSize(200,3000)
     [System.Console]::BackgroundColor = [System.ConsoleColor]::Black
     [System.Console]::Clear()
-    Write-Host "`
-     _   _ _   _______ _  __                  _                                      _`
-    | \ | | | |__   __| |/ /                 (_)                                    | |`
-    |  \| | |    | |  | ' /    ___ _ ____   ___ _ __ ___  _ __  _ __ ___   ___ _ __ | |`
-    | . `` | |    | |  |  <    / _ \ '_ \ \ / / | '__/ _ \| '_ \| '_ `` _ \ / _ \ '_ \| __|`
-    | |\  | |____| |  | . \  |  __/ | | \ V /| | | | (_) | | | | | | | | |  __/ | | | |_`
-    |_| \_|______|_|  |_|\_\  \___|_| |_|\_/ |_|_|  \___/|_| |_|_| |_| |_|\___|_| |_|\__|`
-             | |                                            | |`
-     ___  ___| |_ _   _ _ __    _ __  _ __ ___   ___ ___  __| |_   _ _ __ ___`
-    / __|/ _ \ __| | | | '_ \  | '_ \| '__/ _ \ / __/ _ \/ _`` | | | | '__/ _ \`
-    \__ \  __/ |_| |_| | |_) | | |_) | | | (_) | (_|  __/ (_| | |_| | | |  __/`
-    |___/\___|\__|\__,_| .__/  | .__/|_|  \___/ \___\___|\__,_|\__,_|_|  \___|`
-                       | |     | |`
-                       |_|     |_|`
+    write-host "`
+      _   _ _   _____ _  __                  _                                      _   `
+     | \ | | | |_   _| |/ /   ___ _ ____   _(_)_ __ ___  _ __  _ __ ___   ___ _ __ | |_ `
+     |  \| | |   | | | ' /   / _ \ '_ \ \ / / | '__/ _ \| '_ \| '_ `` _ \ / _ \ '_ \| __|`
+     | |\  | |___| | | . \  |  __/ | | \ V /| | | | (_) | | | | | | | | |  __/ | | | |_ `
+     |_| \_|_____|_| |_|\_\  \___|_| |_|\_/ |_|_|  \___/|_| |_|_| |_| |_|\___|_| |_|\__|`
+                _                                              _                        `
+       ___  ___| |_ _   _ _ __    _ __  _ __ ___   ___ ___  __| |_   _ _ __ ___         `
+      / __|/ _ \ __| | | | '_ \  | '_ \| '__/ _ \ / __/ _ \/ _`` | | | | '__/ _ \        `
+      \__ \  __/ |_| |_| | |_) | | |_) | | | (_) | (_|  __/ (_| | |_| | | |  __/        `
+      |___/\___|\__|\__,_| .__/  | .__/|_|  \___/ \___\___|\__,_|\__,_|_|  \___|        `
+                         |_|     |_|                                                    `
     " -ForegroundColor White
     Add-Type -TypeDefinition "namespace Refresh`n{`n    using System;`n    using System.Linq;`n    using System.Collections;`n    using System.Collections.Generic;`n    using Microsoft.Win32;`n    public class EnvironmentVariables`n    {`n        public static RegistryKey HKLM = Registry.LocalMachine.OpenSubKey(@`"System\CurrentControlSet\Control\Session Manager\Environment`");`n        public static RegistryKey HKCU = Registry.CurrentUser.OpenSubKey(@`"Environment`");`n        public static string ARCH = Environment.GetEnvironmentVariable(`"PROCESSOR_ARCHITECTURE`");`n        public static string USER = Environment.GetEnvironmentVariable(`"USERNAME`");`n        public static void FromRegistry()`n        {`n            string SYSPATH = String.Empty;`n            string USERPATH = String.Empty;`n            string PATHVAR = String.Empty;`n            using(RegistryKey HKLM = Registry.LocalMachine.OpenSubKey(@`"System\CurrentControlSet\Control\Session Manager\Environment`"))`n            {`n                HKLM.GetValueNames().ToList().Where(i=>`n                {`n                    return (i.ToLower() != `"path`");`n                }).ToList().ForEach(i=>`n                {`n                    Environment.SetEnvironmentVariable(i,HKLM.GetValue(i).ToString());`n                });`n                SYSPATH = HKLM.GetValue(`"Path`").ToString() + ((Char)59).ToString();`n            }`n            using(RegistryKey HKCU = Registry.CurrentUser.OpenSubKey(@`"Environment`"))`n            {`n                HKCU.GetValueNames().ToList().Where(i=>`n                {`n                    return (i.ToLower() != `"path`");`n                }).ToList().ForEach(i=>`n                {`n                    Environment.SetEnvironmentVariable(i,HKCU.GetValue(i).ToString());`n                });`n                USERPATH = HKCU.GetValue(`"Path`").ToString();`n            }`n            PATHVAR = SYSPATH + USERPATH;`n            Environment.SetEnvironmentVariable(`"Path`",PATHVAR);`n            Environment.SetEnvironmentVariable(`"PROCESSOR_ARCHITECTURE`",ARCH);`n            Environment.SetEnvironmentVariable(`"USERNAME`",USER);`n        }`n    }`n}`n"
     $NLTK_FOLDER = "C:\.temp\nltk"
